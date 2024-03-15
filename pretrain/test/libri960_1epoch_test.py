121a955acbcb086e7cf4a4e0467acf9b533bcdd3
linkdom <donglinkang2021@163.com>
2024-03-15 15:47:33 +0800

    pretrain libri960 starter adam 3e-4 no scheduler

--- pretrain/test/libri960_1epoch_test.py
+++ pretrain/test/libri960_1epoch_test.py
@@ -10,6 +10,12 @@ print(f"number of batches per epoch: {n_batches}")
 pbar = tqdm(total=n_batches, desc="Processing batches", leave=True, unit="batch")
 for x, y, lx, ly in trainloader:
     x, y, lx, ly = x.to(device), y.to(device), lx.to(device), ly.to(device)
-    pbar.set_description(f"x.shape: {x.shape} y.shape: {y.shape} lx.shape: {lx.shape} ly.shape: {ly.shape}")
+    pbar.set_description(f"x: {x.shape} y: {y.shape} lx: {lx.shape} ly: {ly.shape}")
     pbar.update(1)  
 pbar.close()
+
+"""output
+(GPT) root@asr:~/minasr# python libri960_1epoch_test.py 
+number of batches per epoch: 4395
+x: torch.Size([64, 1671, 80]) y: torch.Size([64, 1671]) lx: torch.Size([64]) ly: torch.Size([64]):   2%|▏         | 79/4395 [00:41<35:28,  2.03batch/s]
+"""
